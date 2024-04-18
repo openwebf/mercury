ba28deeae353ee1940ad935d6cdfe00db2a9b568
ekkis <e@arix.com>
2024-04-17 22:56:09 -0700

    fixed url to call for testing

--- mercury/example/assets/bundle.js
+++ mercury/example/assets/bundle.js
@@ -2,7 +2,7 @@ let seconds = 0;
 
 const hello = async () => {
   // const arix = "XX";
-  const arix = await fetch('www.arix.com');
+  const arix = await fetch('https://www.arix.com/');
   setInterval(() => {
     mercury.dispatcher.dispatch('example', { message: `{arix}: ${seconds} seconds.`});
     seconds++;
